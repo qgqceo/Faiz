b05f7e0785d082c173bdef2755920250ad527cf0
brooklyn1984 <brooklyn1984@qq.com>
2017-07-27 19:03:41 +0800

    修改注册页样式

--- src/views/login/Signup.vue
+++ src/views/login/Signup.vue
@@ -1,21 +1,25 @@
 <template>
-    <div class="container">
-        <div>
-            <div></div>
-            <!-- 输入框区域 -->
-            <div class="input-area">
-                <div class="input-box">
-                    <img src="../../assets/login/login_input_name.png" alt="name_image" />
-                    <input v-model="name" type="text" placeholder="Password" />
+    <div class="container flex center">
+        <div class="flex flex-row">
+            <div class="left-area">Do it first, Do it right, Do it better</div>
+            <div class="right-area">
+                <!-- 输入框区域 -->
+                <div class="input-area">
+                    <h1>Faiz</h1>
+                    <div class="input-box">
+                        <img src="../../assets/login/login_input_name.png" alt="name_image" />
+                        <input v-model="name" type="text" placeholder="Name" />
+                    </div>
+                    <div class="input-box">
+                        <img src="../../assets/login/login_input_email.png" alt="email_image" />
+                        <input v-model="email" type="text" placeholder="Email" />
+                    </div>
+                    <div class="input-box">
+                        <img src="../../assets/login/login_input_password.png" alt="password_image" />
+                        <input v-model="password" type="text" placeholder="Password" />
+                    </div>
                 </div>
-                <div class="input-box">
-                    <img src="../../assets/login/login_input_email.png" alt="email_image" />
-                    <input v-model="email" type="text" placeholder="Email" />
-                </div>
-                <div class="input-box">
-                    <img src="../../assets/login/login_input_password.png" alt="password_image" />
-                    <input v-model="password" type="text" placeholder="Password" />
-                </div>
+                <button class="bg-blue">Sign Up</button>
             </div>
         </div>
     </div>
@@ -34,16 +38,36 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../../styles/base.scss';
 .container {
     width: 100%;
     height: 100%;
     background: url('../../assets/login/signup_back.jpg');
     background-size: cover;
-    position: relative
+    position: relative;
+}
+.left-area {
+    width: 300px;
+    height: 300px;
+    background-color: rgba( 255, 255, 255, 0.3);
+}
+.right-area {
+    height: 300px;
+    padding: 30px 30px 0 30px;
+    box-sizing: border-box;
+    background-color: #fff;
+    h1 {
+        text-align: center;
+        color: #666;
+        margin-bottom: 10px;
+    }
+    button {
+        width: 100%;
+        height: 40px;
+        color: #fff;
+    }
 }
 .input-area {
-    height: 100px;
-    padding: 30px;
     .input-box {
         position: relative;
         img {
